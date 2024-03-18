e473a08b6a180b4a2f4b327a79973a66107441ed
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-03-18 20:40:09 -0300

    Create 14D_Buil_Tools.sh

---
+++ 14D_Buil_Tools.sh
@@ -0,0 +1,13 @@
+sudo apt install -y build-essential \
+libcairo-dev \
+libxkbcommon-x11-dev \
+libxkbcommon-dev \
+libxcb-cursor-dev \
+libxcb-keysyms1-dev \
+libxcb-util-dev \
+libxrandr-dev \
+libxinerama-dev \
+libxcursor-dev \
+libasound2-dev \
+libjack-jackd2-dev \
+cmake
