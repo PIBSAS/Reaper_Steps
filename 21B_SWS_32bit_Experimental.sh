0edfd71b3e214281eaf69788c5193c2147c0b0df
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-03-19 13:53:19 -0300

    Update 21B_SWS_32bit_Experimental.sh

--- 21B_SWS_32bit_Experimental.sh
+++ 21B_SWS_32bit_Experimental.sh
@@ -1,5 +1,5 @@
 cd $HOME/.config/REAPER/
-wget -c https://sws-extension.org/download/pre-release/sws-2.14.0.0-Linux-armv7l-8a12353a.tar.xz
+wget -c "https://www.sws-extension.org/download/pre-release/sws-2.14.0.1-Linux-armv7l-216638bb.tar.xz"
 tar -Jxvf sws*.tar.xz
 rm sws*
 cd
