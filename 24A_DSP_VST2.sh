15a897e6794187b57422c1f9adc2f85c7d4b2744
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-03-19 12:20:41 -0300

    Create 24A_DSP_VST2.sh

---
+++ 24A_DSP_VST2.sh
@@ -0,0 +1,5 @@
+cd
+wget -c https://futurenoize.com/dsp56300/builds/master/DSP56300Emu-${DSP}-Linux_aarch64-Osirus-VST2.deb
+wget -c https://futurenoize.com/dsp56300/builds/master/DSP56300Emu-${DSP}-Linux_aarch64-OsirusFX-VST2.deb
+sudo dpkg -i DSP56300Emu*VST2.deb
+sudo dpkg -i DSP56300Emu*FX-VST2.deb
