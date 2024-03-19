bfc4df55c72da38275d76386253bdf1a6f4fb7fd
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-03-19 12:05:35 -0300

    Create 22_Samplicity.sh

---
+++ 22_Samplicity.sh
@@ -0,0 +1,6 @@
+cd
+mkdir Music
+wget -c "https://archive.org/download/Samplicity/Samplicity_M7_Main-02-Wave32bit-48Khz_v1.1.zip"
+unzip Sampli*.zip
+rm Sampli*.zip
+mv "Samplicity M7 Main - 02 - Wave, 32 bit, 48 Khz, v1.1"/ Music/
