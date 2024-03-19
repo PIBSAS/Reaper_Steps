b2d394377c1b1e5d7c8979fdcda3a6b2d2d2fb8c
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-03-18 21:05:31 -0300

    Create 15B_SurgeXT_Bookworm.sh

---
+++ 15B_SurgeXT_Bookworm.sh
@@ -0,0 +1,25 @@
+cd
+sudo apt install -y build-essential \
+libcairo-dev \
+libxkbcommon-x11-dev \
+libxkbcommon-dev \
+libxcb-cursor-dev \
+libxcb-keysyms1-dev \
+libxcb-util-dev \
+libxrandr-dev \
+libxinerama-dev \
+libxcursor-dev \
+libasound2-dev \
+libjack-jackd2-dev \
+cmake
+git clone https://github.com/surge-synthesizer/surge.git
+cd surge
+git submodule update --init --recursive
+sudo apt install -y clang
+cmake -Bignore/s13clang -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+cmake --build ignore/s13clang --target surge-xt_Standalone --parallel 3
+cd
+cd surge
+cd ignore
+cd s13clang
+sudo make install
