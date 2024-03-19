dc3dba498925eda8c794ca63a8ac0c5decf5f4ab
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-03-19 12:25:15 -0300

    Create 24A_DSP_VirusC.sh

---
+++ 24A_DSP_VirusC.sh
@@ -0,0 +1,3 @@
+cd
+wget -c "https://archive.org/download/access-virus-b-c-roms/Access%20Virus%20C%20%28am29f040b_6v6%29.zip/Access%20Virus%20C%20%28am29f040b_6v6%29.BIN"
+sudo cp "Access Virus C (am29f040b_6v6).BIN" /usr/local/lib/vst/
