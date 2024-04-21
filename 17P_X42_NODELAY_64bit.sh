87931efdc9665b8066ee3c39682e04220f06d0bc
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-04-20 21:46:05 -0300

    Update 17P_X42_NODELAY_64bit.sh
    Change NODELAY to NDL

--- 17P_X42_NODELAY_64bit.sh
+++ 17P_X42_NODELAY_64bit.sh
@@ -1,5 +1,5 @@
 cd
-wget -c https://x42-plugins.com/x42/linux/x42-nodelay-v${NODELAY}-arm64.tar.gz
+wget -c https://x42-plugins.com/x42/linux/x42-nodelay-v${NDL}-arm64.tar.gz
 tar -xzvf x42*.tar.gz
 cd x42*/
 yes | ./install-lv2.sh
